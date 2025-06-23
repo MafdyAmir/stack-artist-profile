c19e63910010f6454f42c74ed01ef51935185698
Mafdy Amir <109095432+MafdyAmir@users.noreply.github.com>
2025-06-23 16:24:21 +0300

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -65,7 +65,8 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
             <Link to="/" className="text-xl font-bold relative group">
-              <span className="relative z-10"><MafdyAmir /></span>
+         <span className="relative z-10">&lt;MafdyAmir /&gt;</span>
+
               <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-primary group-hover:w-full transition-all duration-300"></span>
             </Link>
           </div>
